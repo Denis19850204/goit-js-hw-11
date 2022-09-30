3e60e3003764fdb75cf987ef333a9518111b6cab
Denis19850204 <dg04021985@gmail.com>
2022-09-30 22:36:53 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Notiflix from 'notiflix';
-const axios = require('axios').default;
+const axios = require('axios');
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
