9825bc0608b23d5658bf0ae60e4c4274a02c1434
Denis19850204 <dg04021985@gmail.com>
2022-10-01 11:20:27 +0300

    update 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Notiflix from 'notiflix';
-import axios from 'axios'
+import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -29,8 +29,7 @@ function onSubmitForm(e) {
   }
 
   pageNumber = 1;
-  fetchInfo().then(renderImage)
-  
+  fetchInfo().then(renderImage);
 }
 
 function onLoadMore() {
@@ -47,7 +46,7 @@ function onLoadMore() {
 async function fetchInfo() {
   const url = `https://pixabay.com/api/`;
 
-  return  await axios
+  return await axios
     .get(url, {
       params: {
         key: '30165080-69dc7af91b4e9c1a4c0e45d49',
@@ -60,19 +59,6 @@ async function fetchInfo() {
       },
     })
 
-    // return axios
-    //   .get(url, {
-    //     params: {
-    //       key: '30165080-69dc7af91b4e9c1a4c0e45d49',
-    //       q: `${searchValue}`,
-    //       image_type: 'photo',
-    //       orientation: 'horizontal',
-    //       safesearch: 'true',
-    //       per_page: 40,
-    //       page: `${pageNumber}`,
-    //     },
-    //   })
-
     .then(res => {
       pageNumber += 1;
 
@@ -82,17 +68,16 @@ async function fetchInfo() {
           'Sorry, there are no images matching your search query. Please try again.'
         );
       }
-      else if (res.data.totalHits > 0) {
+
+      if (pageNumber === 2 && res.data.totalHits > 0) {
         Notiflix.Notify.success(
           `Hooray! We found ${res.data.totalHits} images.`
         );
       }
-      console.log(res.data)
+
       return res.data;
     });
 }
-
-
 
 function renderImage(data) {
   const card = data.hits
