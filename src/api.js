972aae230c5bdb6d2c38de827bac1327f1a61388
Denis19850204 <dg04021985@gmail.com>
2022-10-02 20:39:30 +0300

    update 4

---
+++ src/api.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const loadMoreBtn = document.querySelector('.load-more');
+loadMoreBtn.hidden = true;
+
+export default async function fetchInfo(searchValue, pageNumber) {
+  const url = `https://pixabay.com/api/`;
+
+  return await axios
+    .get(url, {
+      params: {
+        key: '30165080-69dc7af91b4e9c1a4c0e45d49',
+        q: `${searchValue}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        per_page: 200,
+        page: `${pageNumber}`,
+      },
+    })
+
+    .then(res => {
+      if (res.data.totalHits < 40) {
+        loadMoreBtn.hidden = true;
+      }
+
+      if (!res.data.totalHits) {
+        loadMoreBtn.hidden = true;
+        Notiflix.Notify.warning(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+
+      if (pageNumber === 1 && res.data.totalHits > 0) {
+        Notiflix.Notify.success(
+          `Hooray! We found ${res.data.totalHits} images.`
+        );
+      }
+
+      return res.data;
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Notiflix from 'notiflix';
-import axios from 'axios';
 
 import renderImage from './render';
+import fetchInfo from './api';
 
 const searchForm = document.querySelector('#search-form');
 const galleryCard = document.querySelector('.gallery');
@@ -29,58 +29,18 @@ function onSubmitForm(e) {
   }
 
   pageNumber = 1;
-  fetchInfo().then(renderImage);
+  fetchInfo(searchValue, pageNumber).then(renderImage);
 }
 
 function onLoadMore() {
-  fetchInfo()
+  pageNumber += 1;
+  fetchInfo(searchValue, pageNumber)
     .then(renderImage)
     .catch(data => {
-      console.log(data);
       loadMoreBtn.hidden = true;
       Notiflix.Notify.info(
         `We're sorry, but you've reached the end of search results.`
       );
-    });
-}
-
-export default async function fetchInfo() {
-  const url = `https://pixabay.com/api/`;
-
-  return await axios
-    .get(url, {
-      params: {
-        key: '30165080-69dc7af91b4e9c1a4c0e45d49',
-        q: `${searchValue}`,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: 'true',
-        per_page: 40,
-        page: `${pageNumber}`,
-      },
-    })
-
-    .then(res => {
-      pageNumber += 1;
-
-      if (res.data.totalHits < 40) {
-        loadMoreBtn.hidden = true;
-      }
-
-      if (!res.data.totalHits) {
-        loadMoreBtn.hidden = true;
-        Notiflix.Notify.warning(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-
-      if (pageNumber === 2 && res.data.totalHits > 0) {
-        Notiflix.Notify.success(
-          `Hooray! We found ${res.data.totalHits} images.`
-        );
-      }
-
-      return res.data;
     });
 }
 
